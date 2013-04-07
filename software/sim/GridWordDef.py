871370378cfedbb77f5d91157d2a6a5a6f3a35ac
mattlokes <matlokes@gmail.com>
2013-04-07 02:27:09 +0100

    Add GridWordDef.py , which contains pixel mappings to words need for time

---
+++ software/sim/GridWordDef.py
@@ -0,0 +1,48 @@
+
+class GridWordDef:
+      #THE TIME IS
+      #----------------------------------------------------------
+      #MIN (ONE)
+      #MIN (TWO)
+      #MIN (THREE)
+      #MIN (FOUR)
+      #MIN (FIVE)
+      #MIN (SIX)
+      #MIN (SEVEN)
+      #MIN (EIGHT)
+      #MIN (NINE)
+      #MIN (TEN)
+      #MIN (ELEVEN)
+      #MIN (TWELEVE)
+      #MIN (THIR)
+      #MIN (TEEN)
+      #QUARTER
+      #TWENTY
+      #HALF
+      #MINUTES
+      #MINUTE
+      #PAST
+      #TOO
+      #----------------------------------------------------------
+      #HOUR (ONE)
+      #HOUR (TWO)
+      #HOUR (THREE)
+      #HOUR (FOUR)
+      #HOUR (FIVE)
+      #HOUR (SIX)
+      #HOUR (SEVEN)
+      #HOUR (EIGHT)
+      #HOUR (NINE)
+      #HOUR (TEN)
+      #HOUR (ELEVEN)
+      #HOUR (TWELEVE)
+      #----------------------------------------------------------
+      #IN THE
+      #AT
+      #MORNING
+      #AFTERNOON
+      #EVENING
+      #NIGHT
+      woo = 0
+
+      
