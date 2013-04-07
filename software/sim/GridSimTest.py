ff5acd18d0985d3b5269111f26d365c156b05dac
mattlokes <matlokes@gmail.com>
2013-04-07 02:30:50 +0100

    Add function placeholder for printing the time

--- software/sim/GridSimTest.py
+++ software/sim/GridSimTest.py
@@ -1,5 +1,22 @@
 from GridInterface_Sim import *
+from GridWordDef import *
+
+def printdiagonal():
+    for i in range(14):
+        gis.setpixel(i,i,255,255,255)
+
+def printclock():
+    gwd = GridWordDef()
+    #LOOP every 3 seconds
+    #GET TIME
+    #PARSE TIME
+    #THE TIME IS
+    #MINUTES
+    #HOUR
+    #MORNING,AFTERNOON, EVENING, NIGHT?
+    pass
+    
 
 gis = GridInterface_Sim(16,14,3,'./grid.conf')
-for i in range(14):
-    gis.setpixel(i,i,255,255,255)
+#printdiagonal()
+printclock()
