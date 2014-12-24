3891c8e2e520cae3c37f3601aceb56c2f4c93098
matt <matlokes@gmail.com>
2014-12-24 21:26:07 +0000

    Create Module Instantiation Loop & Message Routing Prototype

--- software/python/framework/webserver/webServer.py
+++ software/python/framework/webserver/webServer.py
@@ -6,7 +6,7 @@ class webServer ():
   
    serverPath = "framework/webserver/"   
 
-   def start (self):
+   def startup (self):
       if not os.path.isdir(self.serverPath+"temp/") or not os.path.exists(self.serverPath+"temp/"):
          os.mkdir( self.serverPath+"temp/", 0777 )
       subprocess.call(["lighttpd", "-f", "./{0}lighttpd_pi.conf".format(self.serverPath)])
--- software/python/main.py
+++ software/python/main.py
@@ -8,7 +8,7 @@ from framework.webserver.webServer import *
 from interfaces.hwInterface import *
 from interfaces.wsInterface import *
 
-#from applications.clockApp.clockApp import *
+from applications.clockApp.clockApp import *
 from applications.colourTestApp.colourTestApp import *
 
 def sigIntHandler(signal, frame):
@@ -37,20 +37,63 @@ if len(sys.argv) > 1:
    if "-fastRx"     in sys.argv: fastRx=True
    if "-debugRx"    in sys.argv: fastRx=False
 
-wbs = webServer()
-wbs.start()
+modules = {
+           'DISP': 
+           {'class':"hwInterface", 'state': "running", 'obj': None,
+            'txPipe': None, 'rxPipe': None, 'args': [fastRx, fakeSerial] },
+           'WEBSOCK':
+           {'class':"wsInterface", 'state': "running", 'obj': None,
+            'txPipe': None },
+           'WEBSERVER': 
+           {'class':"webServer", 'state': "running", 'obj': None},
+           'CLOCK':
+           {'class':"clockApp", 'state': "running", 'obj': None,
+            'txPipe': None, 'rxPipe': None },
+           'COLTEST': 
+           {'class':"colourTestApp", 'state': None, 'obj': None,
+            'txPipe': None, 'rxPipe': None }
+          }
 
-hw = hwInterface ( fakePipe, txPipe, fastRx, fakeSerial )
-hw.startup()
+#Go through Modules Instantiating them all and starting up
+for name, module in modules.iteritems():
+   argList = []
+   if module['state'] is "running":
+      if 'txPipe' in module.keys():
+         module['txPipe'] = Queue.Queue()
+         argList.append(module['txPipe'])
+      if 'rxPipe' in module.keys():
+         module['rxPipe'] = Queue.Queue()
+         argList.append(module['rxPipe'])
+      if 'args' in module.keys():
+         argList += module['args']
+      module['obj'] = eval(module['class'])(*argList)
+      module['obj'].startup()
 
-cta = colourTestApp(txPipe, rxPipe)
-cta.startup()
+#MAIN LOOP Check for Messages from all modules and route them
+while True:
+   for name, module in modules.iteritems():
+      try:
+         msg = module['txPipe'].get(timeout=0.01)
+      except:
+         pass
+      else:
+         modules[msg['dst']]['rxPipe'].put(msg)
+
+###########################################
+#wbs = webServer()
+#wbs.start()
+
+#hw = hwInterface ( fakePipe, txPipe, fastRx, fakeSerial )
+#hw.startup()
+
+#cta = colourTestApp(txPipe, rxPipe)
+#cta.startup()
 
 #ca = clockApp( txPipe, rxPipe )
 #ca.startup()
 
-ws = wsInterface( rxPipe )
-ws.startup()
+#ws = wsInterface( rxPipe )
+#ws.startup()
 
 while True :
    time.sleep(5)
