6daf3028d41697efce91b31bc934c7caeebe4509
matt <matlokes@gmail.com>
2014-12-27 01:41:31 +0000

    Update Webpage Structure and python webserver module

--- software/python/framework/webserver/webServer.py
+++ software/python/framework/webserver/webServer.py
@@ -9,7 +9,11 @@ class webServer ():
    def startup (self):
       if not os.path.isdir(self.serverPath+"temp/") or not os.path.exists(self.serverPath+"temp/"):
          os.mkdir( self.serverPath+"temp/", 0777 )
-      subprocess.call(["lighttpd", "-f", "./{0}lighttpd_pi.conf".format(self.serverPath)])
+      if os.environ['USER'] is "pi":
+         subprocess.call(["lighttpd", "-f", "./{0}lighttpd_pi.conf".format(self.serverPath)])
+      else:
+         subprocess.call(["lighttpd", "-f", "./{0}lighttpd_pc.conf".format(self.serverPath)])
+      
       #subprocess.call(["pwd"])
    
    def kill(self):
